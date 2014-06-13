938bccea20b7191a2a2f40d8e0bb631985000380
Robert Penczak <rpenczak@verizon.net>
2014-06-12 20:15:32 -0400

    Mod One add test 2

--- test2.php
+++ test2.php
@@ -1,5 +1,5 @@
 <?php
-    echo 'This is a test'; // This is a one-line c++ style comment
+    echo 'This is test#2'; // This is a one-line c++ style comment
     /* This is a multi line comment
        yet another line of comment */
     echo 'This is yet another test';
